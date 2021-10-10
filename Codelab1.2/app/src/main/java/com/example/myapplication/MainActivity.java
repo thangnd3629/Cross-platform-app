e74932071642ae331e1c173221a5208143677a1c
Nguyen Duc Thang <hsgsdominate@gmail.com>
2021-10-10 22:59:53 +0700

    Codelab 1.2

--- Codelab1.2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Codelab1.2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,16 +11,12 @@ public class MainActivity extends AppCompatActivity {
 
     private int counter = 0;
     private TextView showCounter;
-    private Button countButton;
-    private Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showCounter = (TextView) findViewById(R.id.showCounter);
-        countButton = (Button) findViewById(R.id.button3);
-        resetButton = (Button) findViewById(R.id.button);
     }
 
     public void showToastMessage(View view) {
@@ -35,25 +29,5 @@ public class MainActivity extends AppCompatActivity {
         if (showCounter != null) {
             showCounter.setText( Integer.toString(counter) );
         }
-
-        resetButton.setBackgroundColor(Color.BLUE);
-        resetButton.setClickable(true);
-
-        if (counter % 2 == 0) {
-            view.setBackgroundColor(Color.MAGENTA);
-        } else {
-            view.setBackgroundColor(Color.GREEN);
-        }
-    }
-
-    public void resetToZero(View view) {
-        counter = 0;
-        if (showCounter != null) {
-            showCounter.setText( Integer.toString(counter) );
-        }
-
-        view.setClickable(false);
-        view.setBackgroundColor(Color.GRAY);
-        countButton.setBackgroundColor(Color.BLUE);
     }
 }
